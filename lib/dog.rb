d792c2356133284f808bbdbeb0eff2123824784c
Valton Jones <jones.trae@gmail.com>
2021-03-06 12:18:21 -0800

    commit tree 2e5d08309f495bd1431916956dd466faac4dbef2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
+class Dog
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+     self.save
+    end
+    
+    def save
+      @@all << self
+    end
+    def self.all
+      @@all
+    end
+   def self.print_all
+     self.all.each do |pup|
+       puts pup.name
+     end
+   end
+    def self.clear_all
+      @@all.clear
+    end
+  end
+
 # Add your code here
